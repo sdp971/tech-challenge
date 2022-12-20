b8e82615acd66596c39b309d00bcf4595be437d3
sdp971 <doris190485@hotmail.com>
2022-12-20 17:21:24 +0100

    made contact page and updated index, royal-family and asset pages

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const MenuButton = document.getElementsByClassName('menu-bar')[0];
 const navListItems = document.getElementsByClassName('nav-list-items');
-MenuButton.addEventListener('click', function(){
-    for(var i=0;i<navListItems.length;i++)
-    navListItems[i].classList.toggle('active');
+MenuButton.addEventListener('click', function () {
+    for (var i = 0; i < navListItems.length; i++)
+        navListItems[i].classList.toggle('active');
+});
+
+const form = document.getElementById('submit');
+form.addEventListener('click', function () {
+    alert("Your message has been sent successfully.")
 })
